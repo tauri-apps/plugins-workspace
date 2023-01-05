81d3f6ebe870e42073230106531dd41805ae9255
Jonas Kruckenberg <iterpre@protonmail.com>
2023-01-05 17:08:50 +0100

    fmt

--- plugins/stronghold/rollup.config.mjs
+++ plugins/stronghold/rollup.config.mjs
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 import { createConfig } from "../../shared/rollup.config.mjs";
 
 export default createConfig({
-  input: 'guest-js/index.ts',
+  input: "guest-js/index.ts",
   pkg: JSON.parse(
     readFileSync(new URL("./package.json", import.meta.url), "utf8")
   ),
