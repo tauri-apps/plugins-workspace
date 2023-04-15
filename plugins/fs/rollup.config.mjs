d823268b0316baa67e7baf81f562f26bbf2303e5
Lucas Nogueira <lucas@tauri.studio>
2023-04-15 15:29:01 -0300

    revert api changes

--- plugins/fs/rollup.config.mjs
+++ plugins/fs/rollup.config.mjs
@@ -1,48 +1,11 @@
 import { readFileSync } from "fs";
-import { builtinModules } from "module";
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
 
-const input = "guest-js/index.ts";
-const pkg = JSON.parse(
-  readFileSync(new URL("./package.json", import.meta.url), "utf8")
-);
-const external = [/^@tauri-apps\/api/];
+import { createConfig } from "../../shared/rollup.config.mjs";
 
-export default [
-  {
-    input,
-    external: Object.keys(pkg.dependencies || {})
-      .concat(Object.keys(pkg.peerDependencies || {}))
-      .concat(builtinModules)
-      .concat(external),
-    onwarn: (warning) => {
-      throw Object.assign(new Error(), warning);
-    },
-    strictDeprecations: true,
-    output: {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-    },
-    plugins: [typescript({ sourceMap: true })],
-  },
-  {
-    input,
-    onwarn: (warning) => {
-      throw Object.assign(new Error(), warning);
-    },
-    strictDeprecations: true,
-    output: {
-      file: pkg.browser,
-      format: "es",
-      sourcemap: true,
-      entryFileNames: "[name].min.js",
-    },
-    plugins: [
-      resolve(),
-      // terser(),
-      typescript({ sourceMap: true }),
-    ],
-  },
-];
+export default createConfig({
+  input: "guest-js/index.ts",
+  pkg: JSON.parse(
+    readFileSync(new URL("./package.json", import.meta.url), "utf8")
+  ),
+  external: [/^@tauri-apps\/api/],
+});
