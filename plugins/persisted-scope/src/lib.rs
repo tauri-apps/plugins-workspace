fe8b67736abbf683cc4d2f514d69188a04657ab1
FabianLars <fabianlars@fabianlars.de>
2023-04-24 15:31:39 +0200

    fix compilation with asset-protocol feature flag enabled

--- plugins/persisted-scope/src/lib.rs
+++ plugins/persisted-scope/src/lib.rs
@@ -89,7 +89,7 @@ pub fn init<R: Runtime>() -> TauriPlugin<R> {
                     for allowed in &scope.allowed_paths {
                         let allowed = fix_pattern(&ac, allowed);
 
-                        let _ = fs_scope.allow_file(allowed);
+                        let _ = fs_scope.allow_file(&allowed);
                         #[cfg(feature = "protocol-asset")]
                         let _ = asset_protocol_scope.allow_file(&allowed);
                     }
