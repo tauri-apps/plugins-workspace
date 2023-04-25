60a8a5ec6e18661b6590ee530d64bc3387dea76b
FabianLars <fabianlars@fabianlars.de>
2023-04-25 16:04:27 +0200

    simplify string subst

--- .github/sync-to-mirrors.sh
+++ .github/sync-to-mirrors.sh
@@ -113,7 +113,7 @@ while read -r PLUGIN_NAME; do
 			# Add new tags
 			for FULL_TAG in "${COMMIT_TAGS[@]}"; do
 				if [[ "$FULL_TAG" =~ ^"tauri-plugin-$PLUGIN_NAME-" ]]; then
-					TAG_NAME="${FULL_TAG/"tauri-plugin-$PLUGIN_NAME-"/""}"
+					TAG_NAME="${FULL_TAG#"tauri-plugin-$PLUGIN_NAME-"}"
 					echo "Creating tag $TAG_NAME"
 					git tag "${TAG_NAME}" -m "${GIT_CLI_COMMIT_MESSAGE}"
 					git push origin "${TAG_NAME}"
