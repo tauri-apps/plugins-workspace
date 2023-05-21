82b34e91d18cebc097586ee0cba0574e6c7fa27e
Lucas Nogueira <lucas@tauri.studio>
2023-05-21 12:31:21 -0300

    format

--- .scripts/ci/check-license-header.js
+++ .scripts/ci/check-license-header.js
@@ -2,101 +2,117 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-import fs from 'fs'
-import path from 'path'
-import readline from 'readline'
+import fs from "fs";
+import path from "path";
+import readline from "readline";
 
 const header = `Copyright 2019-2023 Tauri Programme within The Commons Conservancy
 SPDX-License-Identifier: Apache-2.0
-SPDX-License-Identifier: MIT`
-const ignoredLicense = '// Copyright 2021 Jonas Kruckenberg'
+SPDX-License-Identifier: MIT`;
+const ignoredLicense = "// Copyright 2021 Jonas Kruckenberg";
 
-const extensions = ['.rs', '.js', '.ts', '.yml', '.swift', '.kt']
-const ignore = ['target', 'templates', 'node_modules', 'gen', 'dist', 'dist-js', '.svelte-kit', 'api-iife.js']
+const extensions = [".rs", ".js", ".ts", ".yml", ".swift", ".kt"];
+const ignore = [
+  "target",
+  "templates",
+  "node_modules",
+  "gen",
+  "dist",
+  "dist-js",
+  ".svelte-kit",
+  "api-iife.js",
+];
 
 async function checkFile(file) {
-  if (extensions.some(e => file.endsWith(e))) {
-    const fileStream = fs.createReadStream(file)
+  if (extensions.some((e) => file.endsWith(e))) {
+    const fileStream = fs.createReadStream(file);
     const rl = readline.createInterface({
       input: fileStream,
-      crlfDelay: Infinity
-    })
+      crlfDelay: Infinity,
+    });
 
-    let contents = ``
-    let i = 0
+    let contents = ``;
+    let i = 0;
     for await (let line of rl) {
       // ignore empty lines, allow shebang, swift-tools-version and bundler license
-      if (line.length === 0 || line.startsWith("#!") || line.startsWith('// swift-tools-version:') || line === ignoredLicense) {
-        continue
+      if (
+        line.length === 0 ||
+        line.startsWith("#!") ||
+        line.startsWith("// swift-tools-version:") ||
+        line === ignoredLicense
+      ) {
+        continue;
       }
 
       // strip comment marker
-      if (line.startsWith('// ')) {
-        line = line.substring(3)
-      } else if (line.startsWith('# ')) {
-        line = line.substring(2)
+      if (line.startsWith("// ")) {
+        line = line.substring(3);
+      } else if (line.startsWith("# ")) {
+        line = line.substring(2);
       }
 
-      contents += line
+      contents += line;
       if (++i === 3) {
-        break
+        break;
       }
-      contents += '\n'
+      contents += "\n";
     }
     if (contents !== header) {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 async function check(src) {
-  const missingHeader = []
+  const missingHeader = [];
 
   for (const entry of fs.readdirSync(src, { withFileTypes: true })) {
-    const p = path.join(src, entry.name)
+    const p = path.join(src, entry.name);
 
     if (entry.isSymbolicLink() || ignore.includes(entry.name)) {
-      continue
+      continue;
     }
 
     if (entry.isDirectory()) {
-      const missing = await check(p)
-      missingHeader.push(...missing)
+      const missing = await check(p);
+      missingHeader.push(...missing);
     } else {
-      const isMissing = await checkFile(p)
+      const isMissing = await checkFile(p);
       if (isMissing) {
-        missingHeader.push(p)
+        missingHeader.push(p);
       }
     }
   }
 
-  return missingHeader
+  return missingHeader;
 }
 
-const [_bin, _script, ...files] = process.argv
+const [_bin, _script, ...files] = process.argv;
 
 if (files.length > 0) {
   async function run() {
-    const missing = []
+    const missing = [];
     for (const f of files) {
-      const isMissing = await checkFile(f)
+      const isMissing = await checkFile(f);
       if (isMissing) {
-        missing.push(f)
+        missing.push(f);
       }
     }
     if (missing.length > 0) {
-      console.log(missing.join('\n'))
-      process.exit(1)
+      console.log(missing.join("\n"));
+      process.exit(1);
     }
   }
 
-  run()
+  run();
 } else {
-  check(path.resolve(new URL(import.meta.url).pathname, '../../..')).then(missing => {
-    if (missing.length > 0) {
-      console.log(missing.join('\n'))
-      process.exit(1)
+  check(path.resolve(new URL(import.meta.url).pathname, "../../..")).then(
+    (missing) => {
+      if (missing.length > 0) {
+        console.log(missing.join("\n"));
+        process.exit(1);
+      }
     }
-  })
+  );
 }
