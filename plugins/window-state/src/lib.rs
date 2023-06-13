55456de5b5423bb5c898e97957c7c73c31faae0c
Amr Bashir <amr.bashir2015@gmail.com>
2023-06-13 15:48:10 +0300

    fix(window-state): manual default implentation (#425)
    * fix(window-state): manual default implentation, closes #421
    
    * Update lib.rs

--- plugins/window-state/src/lib.rs
+++ plugins/window-state/src/lib.rs
@@ -55,7 +55,7 @@ impl Default for StateFlags {
     }
 }
 
-#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 struct WindowState {
     width: f64,
     height: f64,
@@ -65,6 +65,21 @@ struct WindowState {
     visible: bool,
     decorated: bool,
     fullscreen: bool,
+}
+
+impl Default for WindowState {
+    fn default() -> Self {
+        Self {
+            width: Default::default(),
+            height: Default::default(),
+            x: Default::default(),
+            y: Default::default(),
+            maximized: Default::default(),
+            visible: true,
+            decorated: true,
+            fullscreen: Default::default(),
+        }
+    }
 }
 
 struct WindowStateCache(Arc<Mutex<HashMap<String, WindowState>>>);
