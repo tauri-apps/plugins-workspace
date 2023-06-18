b00bbd735022e310987ebdd6e6c2da80cbacfdf5
Lucas Nogueira <lucas@tauri.studio>
2023-06-18 07:20:06 -0300

    fmt

--- plugins/os/src/init.js
+++ plugins/os/src/init.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-__RAW_global_os_api__
+__RAW_global_os_api__;
 
 // eslint-disable-next-line
 window.__TAURI__.os.__eol = __TEMPLATE_eol__;
