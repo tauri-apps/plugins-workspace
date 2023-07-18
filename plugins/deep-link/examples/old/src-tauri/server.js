a8e748e49665c72103a34e9299b72ccd672b7edd
FabianLars <fabianlars@fabianlars.de>
2023-07-18 10:42:20 +0200

    fix license headers

--- plugins/deep-link/examples/old/src-tauri/server.js
+++ plugins/deep-link/examples/old/src-tauri/server.js
@@ -1,24 +1,31 @@
-import http from 'http'
-import fs from 'fs'
-import path from 'path'
-import * as url from 'url'
-const __dirname = url.fileURLToPath(new URL('.',
-  import.meta.url))
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 
-const port = 8125
+import http from "http";
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-http.createServer(function (request, response) {
-  if (request.url === '/.well-known/apple-app-site-association') {
-    // eslint-disable-next-line
-    fs.readFile(path.resolve(__dirname, 'apple-app-site-association'), function (_error, content) {
-      response.writeHead(200)
-      response.end(content, 'utf-8')
-    });
-  } else {
-    response.writeHead(404)
-    response.end()
-  }
+const port = 8125;
 
-}).listen(port)
+http
+  .createServer(function (request, response) {
+    if (request.url === "/.well-known/apple-app-site-association") {
+      // eslint-disable-next-line
+      fs.readFile(
+        path.resolve(__dirname, "apple-app-site-association"),
+        function (_error, content) {
+          response.writeHead(200);
+          response.end(content, "utf-8");
+        }
+      );
+    } else {
+      response.writeHead(404);
+      response.end();
+    }
+  })
+  .listen(port);
 
-console.log(`Server running at http://127.0.0.1:${port}/`)
+console.log(`Server running at http://127.0.0.1:${port}/`);
--- plugins/deep-link/examples/old/src/main.ts
+++ plugins/deep-link/examples/old/src/main.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 import { listen } from "@tauri-apps/api/event";
 import { transformCallback } from "@tauri-apps/api/tauri";
 
