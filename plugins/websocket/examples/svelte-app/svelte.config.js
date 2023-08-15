0784df88e83a67cc395234470c0574b6e1ffdddd
Lucas Nogueira <lucas@tauri.studio>
2023-08-15 11:17:14 -0300

    add example to workspace

--- plugins/websocket/examples/svelte-app/svelte.config.js
+++ plugins/websocket/examples/svelte-app/svelte.config.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
