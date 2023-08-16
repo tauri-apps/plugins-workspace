898d4b32736e0b6645558c49789c6cf2fa245e94
FabianLars <fabianlars@fabianlars.de>
2023-08-16 12:19:27 +0200

    cargo fmt

--- plugins/deep-link/src/commands.rs
+++ plugins/deep-link/src/commands.rs
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use tauri::{AppHandle, command, Runtime, Window, State};
+use tauri::{command, AppHandle, Runtime, State, Window};
 
 use crate::{DeepLink, Result};
 
 #[command]
 pub(crate) async fn get_current<R: Runtime>(
-  _app: AppHandle<R>,
-  _window: Window<R>,
-  deep_link: State<'_, DeepLink<R>>
+    _app: AppHandle<R>,
+    _window: Window<R>,
+    deep_link: State<'_, DeepLink<R>>,
 ) -> Result<Option<Vec<url::Url>>> {
-  deep_link.get_current()
+    deep_link.get_current()
 }
