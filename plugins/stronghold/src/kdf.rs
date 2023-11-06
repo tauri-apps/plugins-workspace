a3d58c86018f3f9b5e8a21e2def32f5118a0546f
FabianLars <fabianlars@fabianlars.de>
2023-11-06 11:27:58 +0100

    license header

--- plugins/stronghold/src/kdf.rs
+++ plugins/stronghold/src/kdf.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use rand_chacha::ChaCha20Rng;
 use rand_core::{RngCore, SeedableRng};
 use std::path::Path;
