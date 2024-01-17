94bf80621a3401ec8d7b9d7a8aa32858be53eac2
FabianLars <fabianlars@fabianlars.de>
2024-01-17 15:54:49 +0100

    fix(upload): Prevent duplicate progress reports when using Promise.all

--- plugins/upload/guest-js/index.ts
+++ plugins/upload/guest-js/index.ts
@@ -15,23 +15,24 @@ async function listenToEventIfNeeded(event: string): Promise<void> {
   if (listening) {
     return await Promise.resolve();
   }
-  return await appWindow
-    .listen<ProgressPayload>(event, ({ payload }) => {
-      const handler = handlers.get(payload.id);
-      if (handler != null) {
-        handler(payload.progress, payload.total);
-      }
-    })
-    .then(() => {
-      listening = true;
-    });
+
+  // We're not awaiting this Promise to prevent issues with Promise.all
+  // the listener will still be registered in time.
+  appWindow.listen<ProgressPayload>(event, ({ payload }) => {
+    const handler = handlers.get(payload.id);
+    if (handler != null) {
+      handler(payload.progress, payload.total);
+    }
+  });
+
+  listening = true;
 }
 
 async function upload(
   url: string,
   filePath: string,
   progressHandler?: ProgressHandler,
-  headers?: Map<string, string>,
+  headers?: Map<string, string>
 ): Promise<void> {
   const ids = new Uint32Array(1);
   window.crypto.getRandomValues(ids);
@@ -59,7 +60,7 @@ async function download(
   url: string,
   filePath: string,
   progressHandler?: ProgressHandler,
-  headers?: Map<string, string>,
+  headers?: Map<string, string>
 ): Promise<void> {
   const ids = new Uint32Array(1);
   window.crypto.getRandomValues(ids);
