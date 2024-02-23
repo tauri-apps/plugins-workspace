a089423ac6603000eb26c1c4162d7089138a46bb
TukanDev <contact@tukandev.com>
2024-02-23 20:21:59 +0000

    Update plugins/clipboard-manager/guest-js/index.ts
    use camelCase here too
    
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>

--- plugins/clipboard-manager/guest-js/index.ts
+++ plugins/clipboard-manager/guest-js/index.ts
@@ -11,7 +11,7 @@
 import { invoke } from "@tauri-apps/api/core";
 
 type ClipResponse = Record<"plainText", { text: string }>;
-type ClipHtmlResponse = Record<"html", { html: string, alt_html: string }>;
+type ClipHtmlResponse = Record<"html", { html: string, altHtml: string }>;
 
 /**
  * Writes plain text to the clipboard.
