f4d52da3711d0ca4b44d383dc981e18cf6a18722
Amr Bashir <amr.bashir2015@gmail.com>
2024-02-25 19:32:53 +0200

    Update plugins/clipboard-manager/src/mobile.rs

--- plugins/clipboard-manager/src/mobile.rs
+++ plugins/clipboard-manager/src/mobile.rs
@@ -41,7 +41,7 @@ impl<R: Runtime> Clipboard<R> {
     }
 
     // Treat HTML as unsupported on mobile until tested
-    pub fn write_html(&self, _kind: ClipKind) -> crate::Result<ClipboardContents> {
+    pub fn write_html(&self, _kind: ClipKind) -> crate::Result<()> {
         Err(crate::Error::Clipboard(
             "Unsupported on this platform".to_string(),
         ))
