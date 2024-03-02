a9fa9dc3cc7e9d29b1a70b83977b6c56b4c207fd
ahmed <ahmedelkhodery2@gmail.com>
2024-03-02 17:12:33 +0200

    ios

---
+++ plugins/store/ios/Tests/PluginTests/PluginTests.swift
@@ -0,0 +1,12 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+import XCTest
+@testable import StorePlugin
+
+final class StorePluginTests: XCTestCase {
+    func testStore() throws {
+        let plugin = StorePlugin()
+    }
+}
