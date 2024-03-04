22f5acf2407e0689f5a7d87f56cc8c8bfb939ebe
Ahmed Elkhoudiry <ahmed@ryinex.com>
2024-03-04 21:02:17 +0200

    feat(store): Add android & iOS support (#1011)
    * update cli
    
    * init android module
    
    * upgdate gitignore
    
    * add desktop and mobile
    
    * android
    
    * ios
    
    * lib
    
    * remove comment
    
    * cargo fmt
    
    * skip empty file creation
    
    * android comments
    
    * apple path
    
    * Discard changes to plugins/store/ios/README.md
    
    * stop auto directories creation
    
    * Update README.md

---
+++ plugins/store/android/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "app.tauri.store"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 19
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
+    implementation(project(":tauri-android"))
+}
