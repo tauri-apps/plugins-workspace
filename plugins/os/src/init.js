cde858a165f73c0e705d28c7ebbfe5221fa2398b
amrbashir <amr.bashir2015@gmail.com>
2024-05-21 03:07:47 +0300

    refactor(os)!: make `platform`, `arch`, `type`, `family`, `version` and `exe_extension` functions sync
    closes #1351

--- plugins/os/src/init.js
+++ plugins/os/src/init.js
@@ -6,5 +6,11 @@
 Object.defineProperty(window, "__TAURI_OS_PLUGIN_INTERNALS__", {
   value: {
     eol: __TEMPLATE_eol__,
+    os_type: __TEMPLATE_os_type__,
+    platform: __TEMPLATE_platform__,
+    family: __TEMPLATE_family__,
+    version: __TEMPLATE_version__,
+    arch: __TEMPLATE_arch__,
+    exe_extension: __TEMPLATE_exe_extension__,
   },
 });
