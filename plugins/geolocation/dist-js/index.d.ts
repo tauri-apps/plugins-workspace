de3ec1e65e17618b68502237c1e71d397d323166
Brendan Allan <brendonovich@outlook.com>
2024-06-20 17:50:44 +0800

    just ship dist-js for now

---
+++ plugins/geolocation/dist-js/index.d.ts
@@ -0,0 +1,4 @@
+import { type PositionOptions, type Position } from "./bindings";
+export declare function watchPosition(options: PositionOptions, cb: (location: Position) => void): Promise<number>;
+export declare const getCurrentPosition: (options: PositionOptions | null) => Promise<import("./bindings").Result<Position, never>>, clearWatch: (channelId: number) => Promise<import("./bindings").Result<null, never>>, checkPermissions: () => Promise<import("./bindings").Result<import("./bindings").PermissionStatus, never>>, requestPermissions: (permissions: import("./bindings").PermissionType[] | null) => Promise<import("./bindings").Result<import("./bindings").PermissionStatus, never>>;
+export type { PermissionState, PermissionStatus, PermissionType, Position, PositionOptions, Coordinates, } from "./bindings";
