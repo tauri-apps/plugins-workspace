697eee7cc18293a09fd385bd906c479476be3623
FabianLars-crabnebula <fabianlars@crabnebula.dev>
2024-07-30 18:08:52 +0200

    rebuild

--- plugins/geolocation/dist-js/bindings.d.ts
+++ plugins/geolocation/dist-js/bindings.d.ts
@@ -7,9 +7,6 @@ export declare const commands: {
     requestPermissions(permissions: PermissionType[] | null): Promise<Result<PermissionStatus, Error>>;
 };
 /** user-defined events **/
-export declare const events: {
-    randomNumber: __EventObj__<number> & ((handle: __WebviewWindow__) => __EventObj__<number>);
-};
 /** user-defined statics **/
 /** user-defined types **/
 export type Coordinates = {
@@ -107,14 +104,6 @@ export type PositionOptions = {
      */
     maximumAge: number;
 };
-export type RandomNumber = number;
-import * as TAURI_API_EVENT from "@tauri-apps/api/event";
-import { type WebviewWindow as __WebviewWindow__ } from "@tauri-apps/api/webviewWindow";
-type __EventObj__<T> = {
-    listen: (cb: TAURI_API_EVENT.EventCallback<T>) => ReturnType<typeof TAURI_API_EVENT.listen<T>>;
-    once: (cb: TAURI_API_EVENT.EventCallback<T>) => ReturnType<typeof TAURI_API_EVENT.once<T>>;
-    emit: T extends null ? (payload?: T) => ReturnType<typeof TAURI_API_EVENT.emit> : (payload: T) => ReturnType<typeof TAURI_API_EVENT.emit>;
-};
 export type Result<T, E> = {
     status: "ok";
     data: T;
@@ -122,4 +111,3 @@ export type Result<T, E> = {
     status: "error";
     error: E;
 };
-export {};
--- plugins/haptics/dist-js/bindings.d.ts
+++ plugins/haptics/dist-js/bindings.d.ts
@@ -6,22 +6,11 @@ export declare const commands: {
     selectionFeedback(): Promise<Result<null, Error>>;
 };
 /** user-defined events **/
-export declare const events: {
-    randomNumber: __EventObj__<number> & ((handle: __WebviewWindow__) => __EventObj__<number>);
-};
 /** user-defined statics **/
 /** user-defined types **/
 export type Error = never;
 export type ImpactFeedbackStyle = "light" | "medium" | "heavy" | "soft" | "rigid";
 export type NotificationFeedbackType = "success" | "warning" | "error";
-export type RandomNumber = number;
-import * as TAURI_API_EVENT from "@tauri-apps/api/event";
-import { type WebviewWindow as __WebviewWindow__ } from "@tauri-apps/api/webviewWindow";
-type __EventObj__<T> = {
-    listen: (cb: TAURI_API_EVENT.EventCallback<T>) => ReturnType<typeof TAURI_API_EVENT.listen<T>>;
-    once: (cb: TAURI_API_EVENT.EventCallback<T>) => ReturnType<typeof TAURI_API_EVENT.once<T>>;
-    emit: T extends null ? (payload?: T) => ReturnType<typeof TAURI_API_EVENT.emit> : (payload: T) => ReturnType<typeof TAURI_API_EVENT.emit>;
-};
 export type Result<T, E> = {
     status: "ok";
     data: T;
@@ -29,4 +18,3 @@ export type Result<T, E> = {
     status: "error";
     error: E;
 };
-export {};
