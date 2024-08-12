6b079cfdd107c94abc2c7300f6af00bac3ff4040
AHQ <ahqsecret@gmail.com>
2024-08-12 15:41:04 +0530

    refactor(android): Remove targetSdk = 34 from every gradle build files (#1649)
    * remove targetSdk
    
    * changelog
    
    * fix md file
    
    * Add the -js ones as well
    
    * remove -js ones

--- plugins/notification/android/build.gradle.kts
+++ plugins/notification/android/build.gradle.kts
@@ -8,8 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 24        
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
