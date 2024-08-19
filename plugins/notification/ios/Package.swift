000276ee29912251526d748582fae0018f67a9bc
Lucas Nogueira <lucas@tauri.app>
2024-08-19 16:21:33 -0300

    fix: explicitly set a minimum macOS version for older Swift versions
    ref https://github.com/tauri-apps/tauri/pull/10687

--- plugins/notification/ios/Package.swift
+++ plugins/notification/ios/Package.swift
@@ -3,33 +3,32 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-
-
 import PackageDescription
 
 let package = Package(
-    name: "tauri-plugin-notification",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "tauri-plugin-notification",
-            type: .static,
-            targets: ["tauri-plugin-notification"]),
-    ],
-    dependencies: [
-        .package(name: "Tauri", path: "../.tauri/tauri-api")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "tauri-plugin-notification",
-            dependencies: [
-                .byName(name: "Tauri")
-            ],
-            path: "Sources")
-    ]
+  name: "tauri-plugin-notification",
+  platforms: [
+    .macOS(.v10_13),
+    .iOS(.v13),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "tauri-plugin-notification",
+      type: .static,
+      targets: ["tauri-plugin-notification"])
+  ],
+  dependencies: [
+    .package(name: "Tauri", path: "../.tauri/tauri-api")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "tauri-plugin-notification",
+      dependencies: [
+        .byName(name: "Tauri")
+      ],
+      path: "Sources")
+  ]
 )
