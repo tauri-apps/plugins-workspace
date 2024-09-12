984110a978774712bad4d746ed06134d54debcd0
Lucas Fernandes Nogueira <lucas@tauri.app>
2024-09-12 11:20:16 -0300

    fix(deep-link): emit new-url event on app load (#1770)

--- plugins/deep-link/examples/app/src-tauri/src/lib.rs
+++ plugins/deep-link/examples/app/src-tauri/src/lib.rs
@@ -12,10 +12,17 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init(|_app, argv, _cwd| {
+    #[allow(unused_mut)]
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|_app, argv, _cwd| {
             println!("single instance triggered: {argv:?}");
-        }))
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_deep_link::init())
         .plugin(
             tauri_plugin_log::Builder::default()
