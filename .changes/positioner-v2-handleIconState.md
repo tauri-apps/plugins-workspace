51df2800f82a60841dabdf294a56bbb4248458db
Jacob Bolda <me@jacobbolda.com>
2024-09-20 23:52:17 -0500

    [positioner] handleIconState in JS

---
+++ .changes/positioner-v2-handleIconState.md
@@ -0,0 +1,6 @@
+---
+"positioner": minor
+"positioner-js": minor
+---
+
+`handleIconState` function for use in JavaScript event handlers. This allows one to update the TrayIcon state through JavaScript and fully create and handle the TrayIcon without requiring Rust (and the side-effect of creating a TrayIcon).
--- plugins/positioner/guest-js/index.ts
+++ plugins/positioner/guest-js/index.ts
@@ -4,6 +4,13 @@
 // SPDX-License-Identifier: MIT
 
 import { invoke } from '@tauri-apps/api/core'
+import type {
+  TrayIconClickEvent,
+  TrayIconEnterEvent,
+  TrayIconEvent,
+  TrayIconLeaveEvent,
+  TrayIconMoveEvent
+} from '@tauri-apps/api/tray'
 
 /**
  * Well known window positions.
@@ -35,5 +42,30 @@ export enum Position {
 export async function moveWindow(to: Position): Promise<void> {
   await invoke('plugin:positioner|move_window', {
     position: to
+  })
+}
+
+export async function handleIconState(event: TrayIconEvent): Promise<void> {
+  if ('click' in event) {
+    await invokeSetTrayIconState(event.click.rect)
+  } else if ('enter' in event) {
+    await invokeSetTrayIconState(event.enter.rect)
+  } else if ('leave' in event) {
+    await invokeSetTrayIconState(event.leave.rect)
+  } else if ('move' in event) {
+    await invokeSetTrayIconState(event.move.rect)
+  }
+}
+
+async function invokeSetTrayIconState(
+  rect:
+    | TrayIconClickEvent['rect']
+    | TrayIconEnterEvent['rect']
+    | TrayIconLeaveEvent['rect']
+    | TrayIconMoveEvent['rect']
+) {
+  await invoke('plugin:positioner|set_tray_icon_state', {
+    position: rect.position.Physical,
+    size: rect.size.Physical
   })
 }
