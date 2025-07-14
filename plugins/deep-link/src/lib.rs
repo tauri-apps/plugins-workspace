06e26a56a7f032e0dd6f1ba8b5ceb2df7c6540d2
Brad <bradleyglaurie@gmail.com>
2025-07-14 08:47:35 +0100

    fix(deep-link): unnecessary second MimeType section removal

--- plugins/deep-link/src/lib.rs
+++ plugins/deep-link/src/lib.rs
@@ -308,7 +308,6 @@ mod imp {
                         let old_mimes = section.remove("MimeType").unwrap_or_default();
 
                         if !old_mimes.split(';').any(|mime| mime == mime_type) {
-                            section.remove("MimeType");
                             section.append("MimeType", format!("{mime_type};{}", old_mimes));
                             desktop_file.write_to_file(&target_file)?;
                         }
