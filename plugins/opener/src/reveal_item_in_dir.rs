23f7eebed1e0b8ccd7fdb6e7bdd8159d8f4aec9a
petersamokhin <peter.samokhin007@gmail.com>
2025-08-05 23:30:56 +0100

    feature: reveal multiple items in dir

--- plugins/opener/src/reveal_item_in_dir.rs
+++ plugins/opener/src/reveal_item_in_dir.rs
@@ -73,8 +73,7 @@ pub fn reveal_items_in_dir<P: AsRef<Path>>(paths: &Vec<P>) -> crate::Result<()> 
 
 #[cfg(windows)]
 mod imp {
-    use super::*;
-    use std::path::Path;
+    use std::path::PathBuf;
 
     use windows::Win32::UI::Shell::Common::ITEMIDLIST;
     use windows::{
@@ -92,7 +91,7 @@ mod imp {
         },
     };
 
-    pub fn reveal_item_in_dir<P: AsRef<Path>>(path: P) -> crate::Result<()> {
+    pub fn reveal_item_in_dir(path: &PathBuf) -> crate::Result<()> {
         let file = dunce::simplified(path);
 
         let _ = unsafe { CoInitialize(None) };
@@ -144,45 +143,78 @@ mod imp {
         Ok(())
     }
 
-    pub fn reveal_items_in_dir<P: AsRef<Path>>(paths: &Vec<P>) -> crate::Result<()> {
+    pub fn reveal_items_in_dir(paths: &[PathBuf]) -> crate::Result<()> {
         if paths.is_empty() {
             return Ok(());
         }
 
         let first_path = dunce::simplified(&paths[0]);
-        let dir = first_path
+        let parent_dir = first_path
             .parent()
             .ok_or_else(|| crate::Error::NoParent(first_path.to_path_buf()))?;
 
-        let _ = unsafe { CoInitialize(None) };
+        // On Windows, SHOpenFolderAndSelectItems requires all items to be in the same directory.
+        // We filter the paths to ensure they all share the same parent as the first path.
+        let files_in_same_dir: Vec<_> = paths
+            .iter()
+            .map(|p| dunce::simplified(p))
+            .filter(|p| p.parent() == Some(parent_dir))
+            .collect();
 
-        let dir_h = HSTRING::from(dir);
-        let dir_item = unsafe { ILCreateFromPathW(&dir_h) };
-
-        let mut items_to_free: Vec<*const ITEMIDLIST> = Vec::with_capacity(paths.len() + 1);
-        items_to_free.push(dir_item);
-
-        let mut file_items: Vec<*const ITEMIDLIST> = Vec::with_capacity(paths.len());
-
-        for path in paths {
-            let simplified_path = dunce::simplified(path);
-            if simplified_path.parent() != Some(dir) {
-                // All items must be in the same directory.
-                // You might want to return an error here.
-                continue;
-            }
-            let file_h = HSTRING::from(simplified_path.as_os_str());
-            let file_item = unsafe { ILCreateFromPathW(&file_h) };
-            file_items.push(file_item);
-            items_to_free.push(file_item);
+        if files_in_same_dir.is_empty() {
+            // This case can happen if the original list had paths from different directories.
+            // We can't open multiple directories, so we do nothing.
+            return Ok(());
         }
 
+        let _ = unsafe { CoInitialize(None) };
+
+        let dir_hstring = HSTRING::from(parent_dir);
+        let dir_item = unsafe { ILCreateFromPathW(&dir_hstring) };
+
+        // Ensure dir_item is freed even if subsequent operations fail.
+        let mut created_file_items = Vec::new();
+
+        for path in &files_in_same_dir {
+            let file_hstring = HSTRING::from(path.as_os_str());
+            let file_item = unsafe { ILCreateFromPathW(&file_hstring) };
+            if !file_item.is_null() {
+                created_file_items.push(file_item);
+            }
+        }
+
+        // The function expects a slice of *const ITEMIDLIST, so we must cast our *mut pointers.
+        let item_id_lists_const: Vec<*const ITEMIDLIST> = created_file_items
+            .iter()
+            .map(|&p| p as *const _)
+            .collect();
+
         let result = unsafe {
-            SHOpenFolderAndSelectItems(dir_item, Some(&file_items), 0).map_err(Into::into)
+            if let Err(e) = SHOpenFolderAndSelectItems(dir_item, Some(&item_id_lists_const), 0) {
+                // Fallback logic from the original function.
+                if e.code().0 == ERROR_FILE_NOT_FOUND.0 as i32 {
+                    let mut info = SHELLEXECUTEINFOW {
+                        cbSize: std::mem::size_of::<SHELLEXECUTEINFOW>() as _,
+                        nShow: SW_SHOWNORMAL.0,
+                        lpFile: PCWSTR(dir_hstring.as_ptr()),
+                        lpVerb: w!("explore"),
+                        ..Default::default()
+                    };
+                    ShellExecuteExW(&mut info).map(|_| ()).map_err(Into::into)
+                } else {
+                    Err(e.into())
+                }
+            } else {
+                Ok(())
+            }
         };
 
-        for item in items_to_free {
-            unsafe { ILFree(Some(item)) };
+        // Free all allocated ITEMIDLISTs
+        unsafe {
+            for item in created_file_items {
+                ILFree(Some(item));
+            }
+            ILFree(Some(dir_item));
         }
 
         result
@@ -199,15 +231,16 @@ mod imp {
 mod imp {
     use super::*;
     use std::collections::HashMap;
+    use std::path::PathBuf;
 
-    pub fn reveal_item_in_dir<P: AsRef<Path>>(path: P) -> crate::Result<()> {
+    pub fn reveal_item_in_dir(path: &PathBuf) -> crate::Result<()> {
         let connection = zbus::blocking::Connection::session()?;
 
-        reveal_with_filemanager1(&vec![path], &connection)
-            .or_else(|_| reveal_with_open_uri_portal(path, &connection))
+        reveal_with_filemanager1(&[path.clone()], &connection)
+            .or_else(|_| reveal_with_open_uri_portal(&path, &connection))
     }
 
-    pub fn reveal_items_in_dir<P: AsRef<Path>>(paths: &Vec<P>) -> crate::Result<()> {
+    pub fn reveal_items_in_dir(paths: &[PathBuf]) -> crate::Result<()> {
         let connection = zbus::blocking::Connection::session()?;
 
         reveal_with_filemanager1(paths, &connection).or_else(|e| {
@@ -220,8 +253,8 @@ mod imp {
         })
     }
 
-    fn reveal_with_filemanager1<P: AsRef<Path>>(
-        paths: &Vec<P>,
+    fn reveal_with_filemanager1(
+        paths: &[PathBuf],
         connection: &zbus::blocking::Connection,
     ) -> crate::Result<()> {
         let uris: Result<Vec<_>, _> = paths
@@ -277,13 +310,13 @@ mod imp {
 
 #[cfg(target_os = "macos")]
 mod imp {
-    use super::*;
     use objc2_app_kit::NSWorkspace;
     use objc2_foundation::{NSArray, NSString, NSURL};
+    use std::path::PathBuf;
 
-    pub fn reveal_item_in_dir<P: AsRef<Path>>(path: P) -> crate::Result<()> {
+    pub fn reveal_item_in_dir(path: &PathBuf) -> crate::Result<()> {
         unsafe {
-            let path = path.as_ref().to_string_lossy();
+            let path = path.to_string_lossy();
             let path = NSString::from_str(&path);
             let urls = vec![NSURL::fileURLWithPath(&path)];
             let urls = NSArray::from_retained_slice(&urls);
@@ -295,12 +328,12 @@ mod imp {
         Ok(())
     }
 
-    pub fn reveal_items_in_dir<P: AsRef<Path>>(paths: &Vec<P>) -> crate::Result<()> {
+    pub fn reveal_items_in_dir(paths: &[PathBuf]) -> crate::Result<()> {
         unsafe {
             let mut urls = Vec::new();
 
             for path in paths.iter() {
-                let path = path.as_ref().to_string_lossy();
+                let path = path.to_string_lossy();
                 let path = NSString::from_str(&path);
                 let url = NSURL::fileURLWithPath(&path);
 
