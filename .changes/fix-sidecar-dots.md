8bee93e79a3a593d4249f888e392cf6da9b243c6
Lucas Nogueira <lucas@tauri.app>
2025-08-24 11:27:59 -0300

    fix(shell): run sidecar with dots in filename, closes #2310

---
+++ .changes/fix-sidecar-dots.md
@@ -0,0 +1,6 @@
+---
+"shell": patch
+"shell-js": patch
+---
+
+Fix sidecar with dots in the filename not working on Windows.
--- plugins/shell/src/process/mod.rs
+++ plugins/shell/src/process/mod.rs
@@ -115,10 +115,18 @@ pub struct Output {
     pub stderr: Vec<u8>,
 }
 
+#[cfg(windows)]
+fn append_extension(path: &Path, ext: &str) -> PathBuf {}
+
 fn relative_command_path(command: &Path) -> crate::Result<PathBuf> {
     match platform::current_exe()?.parent() {
         #[cfg(windows)]
-        Some(exe_dir) => Ok(exe_dir.join(command).with_extension("exe")),
+        Some(exe_dir) => {
+            let mut command_path = exe_dir.join(command);
+            // do not use with_extension to retain dots in the command filename
+            command_path.as_mut_os_string().push(".exe");
+            Ok(command_path)
+        }
         #[cfg(not(windows))]
         Some(exe_dir) => Ok(exe_dir.join(command)),
         None => Err(crate::Error::CurrentExeHasNoParent),
@@ -133,6 +141,10 @@ impl From<Command> for StdCommand {
 
 impl Command {
     pub(crate) fn new<S: AsRef<OsStr>>(program: S) -> Self {
+        log::debug!(
+            "Creating sidecar {}",
+            program.as_ref().to_str().unwrap_or("")
+        );
         let mut command = StdCommand::new(program);
 
         command.stdout(Stdio::piped());
@@ -453,6 +465,23 @@ fn spawn_pipe_reader<F: Fn(Vec<u8>) -> CommandEvent + Send + Copy + 'static>(
 mod tests {
     #[cfg(not(windows))]
     use super::*;
+
+    #[test]
+    fn relative_command_path_resolves() {
+        let cwd_parent = platform::current_exe()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .to_owned();
+        assert_eq!(
+            relative_command_path(Path::new("Tauri.Example")).unwrap(),
+            cwd_parent.join(if cfg!(windows) {
+                "Tauri.Example.exe"
+            } else {
+                "Tauri.Example"
+            })
+        );
+    }
 
     #[cfg(not(windows))]
     #[test]
