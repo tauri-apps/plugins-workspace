tauri-apps/plugins-workspace

commit 21d721a0c2731fc201872f5b99ea8bbdc61b0b60

fix(deep-link): Add command name to linux errors (#2928)

plugins/deep-link/src/error.rs
@@ -23,6 +23,9 @@ pub enum Error {
     #[cfg(target_os = "linux")]
     #[error(transparent)]
     ParseIni(#[from] ini::ParseError),
+    #[cfg(target_os = "linux")]
+    #[error("Failed to run OS command `{0}`: {1}")]
+    Execute(&'static str, #[source] std::io::Error),
     #[cfg(mobile)]
     #[error(transparent)]
     PluginInvoke(#[from] tauri::plugin::mobile::PluginInvokeError),

plugins/deep-link/src/lib.rs
@@ -254,6 +254,7 @@ mod imp {
         ///
         /// ## Platform-specific:
         ///
+        /// - **Linux**: Needs the `xdg-mime` and `update-desktop-database` commands available on the system.
         /// - **macOS / Android / iOS**: Unsupported, will return [`Error::UnsupportedPlatform`](`crate::Error::UnsupportedPlatform`).
         pub fn register<S: AsRef<str>>(&self, _protocol: S) -> crate::Result<()> {
             #[cfg(windows)]
@@ -332,11 +333,13 @@ mod imp {
 
                 Command::new("update-desktop-database")
                     .arg(target)
-                    .status()?;
+                    .status()
+                    .map_err(|error| crate::Error::Execute("update-desktop-database", error))?;
 
                 Command::new("xdg-mime")
                     .args(["default", &file_name, mime_type.as_str()])
-                    .status()?;
+                    .status()
+                    .map_err(|error| crate::Error::Execute("xdg-mime", error))?;
 
                 Ok(())
             }
@@ -405,6 +408,7 @@ mod imp {
         ///
         /// ## Platform-specific:
         ///
+        /// - **Linux**: Needs the `xdg-mime` command available on the system.
         /// - **macOS / Android / iOS**: Unsupported, will return [`Error::UnsupportedPlatform`](`crate::Error::UnsupportedPlatform`).
         pub fn is_registered<S: AsRef<str>>(&self, _protocol: S) -> crate::Result<bool> {
             #[cfg(windows)]
@@ -439,7 +443,8 @@ mod imp {
                         "default",
                         &format!("x-scheme-handler/{}", _protocol.as_ref()),
                     ])
-                    .output()?;
+                    .output()
+                    .map_err(|error| crate::Error::Execute("xdg-mime", error))?;
 
                 Ok(String::from_utf8_lossy(&output.stdout).contains(&file_name))
             }

commit cd9dcb5bae414a463301464d0ca0c5117e1c2ec9

fix(deep-link): revert the breaking change introduced by #2928

plugins/deep-link/src/error.rs
@@ -23,12 +23,19 @@ pub enum Error {
     #[cfg(target_os = "linux")]
     #[error(transparent)]
     ParseIni(#[from] ini::ParseError),
-    #[cfg(target_os = "linux")]
-    #[error("Failed to run OS command `{0}`: {1}")]
-    Execute(&'static str, #[source] std::io::Error),
     #[cfg(mobile)]
     #[error(transparent)]
     PluginInvoke(#[from] tauri::plugin::mobile::PluginInvokeError),
+}
+
+// TODO(v3): change this into an error in v3,
+// see <https://github.com/tauri-apps/plugins-workspace/pull/2970#issuecomment-3244660138>.
+#[inline]
+#[cfg(target_os = "linux")]
+pub(crate) fn inspect_command_error<'a>(command: &'a str) -> impl Fn(&std::io::Error) + 'a {
+    move |e| {
+        tracing::error!("Failed to run OS command `{command}`: {e}");
+    }
 }
 
 impl Serialize for Error {

plugins/deep-link/src/lib.rs
@@ -334,12 +334,14 @@ mod imp {
                 Command::new("update-desktop-database")
                     .arg(target)
                     .status()
-                    .map_err(|error| crate::Error::Execute("update-desktop-database", error))?;
+                    .inspect_err(crate::error::inspect_command_error(
+                        "update-desktop-database",
+                    ))?;
 
                 Command::new("xdg-mime")
                     .args(["default", &file_name, mime_type.as_str()])
                     .status()
-                    .map_err(|error| crate::Error::Execute("xdg-mime", error))?;
+                    .inspect_err(crate::error::inspect_command_error("xdg-mime"))?;
 
                 Ok(())
             }
@@ -444,7 +446,7 @@ mod imp {
                         &format!("x-scheme-handler/{}", _protocol.as_ref()),
                     ])
                     .output()
-                    .map_err(|error| crate::Error::Execute("xdg-mime", error))?;
+                    .inspect_err(crate::error::inspect_command_error("xdg-mime"))?;
 
                 Ok(String::from_utf8_lossy(&output.stdout).contains(&file_name))
             }