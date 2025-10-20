0d941f1ea5cc9afbe59e9753d28f2bcb5ff051aa
Daniel Mader <daniel.mader@impierce.com>
2025-10-20 12:56:55 +0200

    feat(barcode-scanner): update `androidx.camera` from `1.1.0` to `1.5.1`

--- plugins/barcode-scanner/android/build.gradle.kts
+++ plugins/barcode-scanner/android/build.gradle.kts
@@ -33,16 +33,14 @@ android {
 }
 
 dependencies {
-
+    val camerax_version = "1.5.1"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.camera:camera-core:1.1.0")
-    implementation("androidx.camera:camera-view:1.1.0")
-    implementation("androidx.camera:camera-lifecycle:1.1.0")
-    implementation("androidx.camera:camera-camera2:1.1.0")
-    implementation("androidx.camera:camera-lifecycle:1.1.0")
-    implementation("androidx.camera:camera-view:1.1.0")
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
     implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
