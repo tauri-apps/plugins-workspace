e34385365f4f7d26422285d4e2d62a5876a50408
Xuhui Zheng <2529677678@qq.com>
2026-03-09 09:48:56 +0800

    Update plugins/recent-doc/build.rs
    Co-authored-by: Tony <68118705+Legend-Master@users.noreply.github.com>

--- plugins/recent-doc/build.rs
+++ plugins/recent-doc/build.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-const COMMANDS: &[&str] = &["execute"];
+const COMMANDS: &[&str] = &["add_recent_document", "get_recent_documents", "clear_recent_documents"];
 
 fn main() {
     let result = tauri_plugin::Builder::new(COMMANDS)
