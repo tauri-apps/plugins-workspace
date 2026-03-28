397b364d78233365a4b87c4e2fc080b0d5659313
Kokoro2336 <2529677678@qq.com>
2026-03-29 01:10:46 +0800

    chore: fix build.rs.

--- plugins/recent-doc/build.rs
+++ plugins/recent-doc/build.rs
@@ -9,14 +9,7 @@ const COMMANDS: &[&str] = &[
 ];
 
 fn main() {
-    let result = tauri_plugin::Builder::new(COMMANDS)
+    tauri_plugin::Builder::new(COMMANDS)
         .global_api_script_path("./api-iife.js")
-        .android_path("android")
-        .ios_path("ios")
-        .try_build();
-
-    // when building documentation for Android the plugin build result is always Err() and is irrelevant to the crate documentation build
-    if !(cfg!(docsrs) && std::env::var("TARGET").unwrap().contains("android")) {
-        result.unwrap();
-    }
+        .build();
 }
