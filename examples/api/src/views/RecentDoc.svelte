c13bbee5979b68acaf2e584a719a954301b6a6ee
Kokoro2336 <2529677678@qq.com>
2026-03-30 01:02:28 +0800

    feat: add recent-doc to examples/api.

---
+++ examples/api/src/views/RecentDoc.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { addRecentDocument, getRecentDocuments, clearRecentDocuments } from '@tauri-apps/plugin-recent-doc'
+  import { open } from '@tauri-apps/plugin-dialog'
+
+  export let onMessage
+</script>
+
+<div class="flex flex-col gap-2">
+  <button
+    class="btn"
+    on:click={async () => {
+      try {
+        const file = await open({
+          multiple: false,
+          title: 'Select a file to add to recent documents'
+        })
+        if (file) {
+          await addRecentDocument(file)
+          onMessage(`Added "${file}" to recent documents.`)
+        } else {
+          onMessage('No file selected.')
+        }
+      } catch (e) {
+        onMessage(e)
+      }
+    }}>Add document to recent list</button
+  >
+  <button
+    class="btn"
+    on:click={async () => {
+      try {
+        const files = await getRecentDocuments()
+        onMessage(files)
+      } catch (e) {
+        onMessage(e)
+      }
+    }}>Get recent documents</button
+  >
+  <button
+    class="btn"
+    on:click={async () => {
+      try {
+        await clearRecentDocuments()
+        onMessage('Cleared recent documents.')
+      } catch (e) {
+        onMessage(e)
+      }
+    }}>Clear recent documents</button
+  >
+</div>
