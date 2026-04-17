tauri-apps/plugins-workspace

commit 28048039496e84b46847c008416d341f1349e30e

fix: deep-link clippy warnings (#3143)
* Update build.rs

* fix: address clippy warnings in deep-link module

* fix: add deep-link-js patch to address clippy warnings

plugins/deep-link/build.rs
@@ -137,13 +137,7 @@ fn main() {
             let deep_link_domains = config
                 .mobile
                 .iter()
-                .filter_map(|domain| {
-                    if domain.is_app_link() {
-                        return None;
-                    }
-
-                    Some(domain)
-                })
+                .filter(|domain| domain.is_app_link())
                 .collect::<Vec<_>>();
 
             if deep_link_domains.is_empty() {
@@ -177,7 +171,7 @@ fn main() {
                                 );
                                 dict.insert(
                                     "CFBundleURLName".into(),
-                                    format!("{}", domain.scheme[0]).into(),
+                                    domain.scheme[0].clone().into(),
                                 );
                                 plist::Value::Dictionary(dict)
                             })

commit e6cdc9f52e2cd975b11b8e4c12879d597f1f76c3

fix(deep-link): inverted is app link filter (#3396)
Fix #3392, regression from #3143

plugins/deep-link/build.rs
@@ -139,7 +139,7 @@ fn main() {
             let deep_link_domains = config
                 .mobile
                 .iter()
-                .filter(|domain| domain.is_app_link())
+                .filter(|domain| !domain.is_app_link())
                 .collect::<Vec<_>>();
 
             if deep_link_domains.is_empty() {