2d20422fbd7e0f67214d287ce36959ee8279d656
Tony <legendmastertony@gmail.com>
2026-07-16 10:57:25 +0800

    Use MaterialAlertDialogBuilder

--- plugins/dialog/android/build.gradle.kts
+++ plugins/dialog/android/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
