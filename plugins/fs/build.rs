5ee607926451b8e72e299a013c4ec70061ea7c6a
Lucas Nogueira <lucas@tauri.app>
2026-09-21 15:30:52 -0300

    refactor(fs)!: remove the unwatch permission
    There has been no `unwatch` command since v2.0 (`Watcher.close()` releases
    the resource), so `allow-unwatch` and `deny-unwatch` never granted anything.

--- plugins/fs/build.rs
+++ plugins/fs/build.rs
@@ -99,8 +99,6 @@ const COMMANDS: &[&str] = &[
     "fstat",
     "exists",
     "watch",
-    // TODO: Remove this in v3
-    "unwatch",
     "size",
     "start_accessing_security_scoped_resource",
     "stop_accessing_security_scoped_resource",
